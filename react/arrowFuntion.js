63b2c11c6ddb8ef2f4e670db91a1b9b7b7ff212d
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-01-31 10:35:23 +0530

    Day1-ES6

---
+++ react/arrowFuntion.js
@@ -0,0 +1,28 @@
+
+// function my_fun(){
+//     console.log('I am function');
+// }
+
+
+// var my_fun = function(){
+//     console.log('I am function');
+//  this
+// }
+
+
+
+    
+var my_function  =(a,b)=>{
+    console.log
+    console.log('I am an Arrow Function')
+    return a+b;
+}
+
+
+
+const add = (a,b)=>a+b;
+
+const sum = my_function(2,3)
+const total = add(2,6)
+
+console.log(sum,total)
--- react/es6.js
+++ react/es6.js
@@ -17,3 +17,37 @@ a = 10
 // p= "Monika"
 
 console.log(x,y,a,p)
+
+
+//global Scope
+    // Functional Scope
+        // Block Scope
+        
+        var x = 2
+        let y = 3
+        const z= 5
+        
+        function abc(){
+            var p="a";
+            let q="b";
+            const r= "c";
+            
+            console.log(x,y,z)
+            if(y>x){
+                var m="m";
+                let n="n";
+                const o= "o";
+                console.log(x,y,z,p,q,r,m,n,o)
+            }
+             console.log(m,n,o)
+        }
+        
+        abc()
+        // console.log(p,q,r)
+                
+        let x = 2;
+        let name = "Guptacdcwcfeecf"
+        const temp = `Utkarsh ${x+2} ${name}
+        jjhsajds
+        `
+        console.log(temp)
