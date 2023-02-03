18c8850191d1c843cc8c913e46b3aa0b3f2a968d
Utkarsh Gupta <utkarsh.gupta1@housing.com>
2023-02-03 21:57:46 +0530

    react app intro

--- react/learning-app/src/App.js
+++ react/learning-app/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg'
-import './App.css'
+import './style.css'
 
 function App () {
+  const name = 'Utkarsh Gupta'
+  const age = 24
+
+  const person = {
+    name: 'Fahad',
+    occupation: 'Hr',
+    city: 'Bhopal'
+  }
+
+  const students = [
+    {
+      name: 'Shivam',
+      age: 23
+    },
+    {
+      name: 'Riyaz',
+      age: 22
+    },
+    {
+      name: 'Tara',
+      age: 24
+    },
+    {
+      name: 'Shazan',
+      age: 21
+    },
+    {
+      name: 'Zabi',
+      age: 20
+    }
+  ]
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Utkarsh Gupta
-        </a>
-      </header>
+    <div>
+      <h1>Hello,</h1>
+      <h2>We are here</h2>
+      <sub>To</sub>
+      <h3>learn</h3>
+      <h1>React</h1>
+      {name + ' ' + age}
+      <div className='container'>
+        <span>{person.name}</span> <span>{person.occupation}</span>{' '}
+        <span>{person.city}</span>
+      </div>
+      <ul>
+        {students.map((item, index) => {
+          return (
+            <li>
+              {item.name} {item.age}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
 
 export default App
+
+// JSX: Javascript Syntax Extensions
